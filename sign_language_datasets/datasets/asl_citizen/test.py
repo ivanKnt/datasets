82876bd6f7379764a1c22859730a07b88be7da12
ivan Kounte <ivankounte@gmail.com>
2024-04-22 10:18:54 +0000

    changed STRIDE value to be explicit to 1

--- sign_language_datasets/datasets/asl_citizen/test.py
+++ sign_language_datasets/datasets/asl_citizen/test.py
@@ -3,6 +3,6 @@ from pose_format import Pose
 data_buffer = open("5983320426555474-LICENSE.pose", "rb").read()
 pose = Pose.read(data_buffer)
 
-numpy_data = pose.body.data
+numpy_data = pose.body.fps
 confidence_measure  = pose.body.confidence
-print("Shape of Pose Data:", numpy_data.shape)
+print("Shape of Pose Data:", numpy_data)
