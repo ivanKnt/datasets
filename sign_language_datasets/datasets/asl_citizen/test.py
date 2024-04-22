e1f6b1a81baa8a248d618ced0455b294d59b00b0
ivan Kounte <ivankounte@gmail.com>
2024-04-22 09:52:11 +0000

    changed shape of numpy data

---
+++ sign_language_datasets/datasets/asl_citizen/test.py
@@ -0,0 +1,8 @@
+from pose_format import Pose
+
+data_buffer = open("5983320426555474-LICENSE.pose", "rb").read()
+pose = Pose.read(data_buffer)
+
+numpy_data = pose.body.data
+confidence_measure  = pose.body.confidence
+print("Shape of Pose Data:", numpy_data.shape)
